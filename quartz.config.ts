592827b5aceb9f7b2b08864aaf2486271d90c0aa
tkancf <18424095+tkancf@users.noreply.github.com>
2024-11-27 22:57:45 +0900

    Quartz sync: Nov 27, 2024, 10:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🐟️ note.tkancf.com",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
