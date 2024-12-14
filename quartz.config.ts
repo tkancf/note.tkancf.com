4d9fb4954f37747627df526c61d25a92158e86bd
tkancf <18424095+tkancf@users.noreply.github.com>
2024-12-14 20:13:01 +0900

    Update 2024-12-14 20:13:01

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "🐟️ TKN",
     pageTitleSuffix: "",
-    enableSPA: false,
+    enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
